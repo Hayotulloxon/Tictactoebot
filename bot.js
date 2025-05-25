cc78a8a0b0fec5a1f1e53e0a68b0758c431b2eac
Hayotulloxon <143584296+Hayotulloxon@users.noreply.github.com>
2025-05-25 12:46:52 +0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,8 +1,9 @@
 const { Telegraf, Markup } = require('telegraf');
+const express = require('express'); // <-- ping uchun express
 
-// O'zingizning bot tokeningiz va sayt manzilingizni shu yerga yozing
-const BOT_TOKEN = '7455849469:AAEAJPNHEWTGiwFztNl3qxYgDXe5FGcve84'; // <-- O'zgartiring!
-const GAME_URL = 'https://hayotulloxon.github.io/tictactoe/'; // <-- O'zgartiring!
+// Bot token va sayt manzili
+const BOT_TOKEN = '7455849469:AAEAJPNHEWTGiwFztNl3qxYgDXe5FGcve84';
+const GAME_URL = 'https://hayotulloxon.github.io/tictactoe/';
 
 const bot = new Telegraf(BOT_TOKEN);
 
@@ -25,5 +26,15 @@ bot.command('game', (ctx) => {
 });
 
 bot.launch();
-
 console.log("Bot ishga tushdi!");
+
+// ==== Express miniserver (ping uchun) ====
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Tic Tac Toe Telegram bot ishlayapti!");
+});
+app.listen(port, () => {
+  console.log(`Web server http://localhost:${port} da ishlamoqda`);
+});
